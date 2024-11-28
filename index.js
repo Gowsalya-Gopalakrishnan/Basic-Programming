3e25b32b09643809e47bebef89b86e9abcd0e497
Gowsalya-Gopalakrishnan <gowsalyagopalakrishnan1515@gmail.com>
2024-11-28 22:29:25 +0530

    prime number

---
+++ index.js
@@ -0,0 +1,9 @@
+//check the prime number or not 
+
+function checkPrimeNumber(num){
+    if((num>1)&&(num/1)&&(num/num))
+        return true;
+    else
+        return false;
+}
+console.log(checkPrimeNumber(5));
